b86ff2b1dd2a400ae9edc0bce294161e62532e5e
katiemarriner <kemarriner@gmail.com>
2018-09-02 22:01:09 -0400

    vuerouter

---
+++ src/views/StateDetail.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  v-container
+    v-layout(row)
+      v-flex(xs2) State
+</template>
+
+
+<script>
+export default {
+  name: 'StateDetail',
+  mounted() {
+    
+  }
+}
+</script>
---
+++ src/views/StatesList.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  v-container
+    v-layout(row)
+      v-flex(xs6)
+        v-list(two-line)
+          template(
+            v-for="state in states"
+            v-if="statefaceKey[state.abbreviation]"
+          )
+            v-list-tile(
+              :key="state.abbrevation"
+              @click="navigate(state.abbreviation)"
+            )
+              v-list-tile-content(:key="`row-${state.abbreviation}`")
+                v-list-tile-title {{ state.name }}
+              v-list-tile-avatar
+                span.stateface.stateface--large(v-html="statefaceKey[state.abbreviation]")
+            v-divider
+</template>
+
+<script>
+import states from './../assets/states_titlecase.json'
+import statefaceKey from './../assets/stateface.json'
+
+export default {
+  name: 'StatesList',
+  data() {
+    return {
+      states,
+      statefaceKey,
+    }
+  },
+  mounted() {
+    console.log(this.statefaceKey)
+  },
+  computed: {
+    statefaceCode(s) {
+      console.log(s);
+    },
+  },
+  methods: {
+    navigate(abbr) {
+      this.$router.push({
+        name: 'detail',
+        params: {
+          id: abbr
+        }
+      })
+    }
+  }
+}
+</script>
+
